68e5dd1794bc6f09080978b574c802842049fec0
Paul-ncu <65542268+Paul-ncu@users.noreply.github.com>
2021-07-07 20:51:32 +0800

    Update QiniuUtil.java

--- src/main/java/com/ncu/qiniu/QiniuUtil.java
+++ src/main/java/com/ncu/qiniu/QiniuUtil.java
@@ -5,8 +5,8 @@ import com.qiniu.util.Auth;
 public class QiniuUtil {
 	
 	public static String getUptoken() {
-		String accessKey = "62PRI4BzVmqSqNsT_oEcbxCweKb_x0dKnLG0y5pw";
-		String secretKey = "YX30xhjzufAICvzXRDPSZ6n03W1YbEL1Ius3dLLd";
+		String accessKey = "your accessKey";
+		String secretKey = "your secretkey";
 		String bucket = "zzj-works";
 		// String key = "file key";
 		Auth auth = Auth.create(accessKey, secretKey);
